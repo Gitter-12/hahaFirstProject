acc89409b009b4f3413454a00b097da11fb2a2b6
Gitter-12 <ahmadshadid28@gmail.com>
2025-10-12 21:55:20 +0300

    Initial commit

---
+++ add.php
@@ -0,0 +1,8 @@
+<?php
+
+
+    include("db.php");
+
+
+// echo "{$_POST["title"]} <br>";
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action = "add.php" method="post">
+
+
+    <label>Task title: </label>
+    <input type="text" name="title">
+    <input type = "submit" value = "Create Task">
+
+</form>
+
+
+</body>
+
+
+</html>
+
+
+cd "C:\wamp64\www\firstProject"
+git init
+git add .
+git commit -m "Initial commit"
